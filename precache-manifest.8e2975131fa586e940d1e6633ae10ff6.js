96126a8b2b37d205a667853221ada40733b90644
Justin Blankenship <jblanken@unca.edu>
2020-02-20 16:25:31 -0500

    Updates

---
+++ precache-manifest.8e2975131fa586e940d1e6633ae10ff6.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e10a6ccd196d7f47679e6c853c1f7a40",
+    "url": "https://justin-blankenship.github.io/MAVIS/index.html"
+  },
+  {
+    "revision": "de6b7f8f3bbad70549be",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/css/2.47e06e2e.chunk.css"
+  },
+  {
+    "revision": "3fb149aabed4ce7be851",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/css/main.466ee109.chunk.css"
+  },
+  {
+    "revision": "de6b7f8f3bbad70549be",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/js/2.9734be39.chunk.js"
+  },
+  {
+    "revision": "bf23039b4e70a07c5aeb1b4010df3572",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/js/2.9734be39.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3fb149aabed4ce7be851",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/js/main.432a3b31.chunk.js"
+  },
+  {
+    "revision": "4ec471b96947ad01fe49",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/js/runtime-main.8bc02498.js"
+  },
+  {
+    "revision": "b49f7a36fff1db4d94da4fe78d20c25b",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/firstQuarter.b49f7a36.png"
+  },
+  {
+    "revision": "9ea471b751c187f96be3dc01e6c04b40",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/fullMoon.9ea471b7.png"
+  },
+  {
+    "revision": "b7e42a41351c8fbe165eb72dbb90a79c",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/home.b7e42a41.png"
+  },
+  {
+    "revision": "2b10a86336d54dc1aa5d30701c35ab11",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/mavisLogo.2b10a863.png"
+  },
+  {
+    "revision": "76cb061b1464a23a6a77f7898f91ac06",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/shot3.76cb061b.png"
+  },
+  {
+    "revision": "b7c06d6eaff7b70ab0e866374c71cb3d",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/stormImage.b7c06d6e.jpg"
+  },
+  {
+    "revision": "e59de20d360aefdad0b381da5985f619",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/thirdQuarter.e59de20d.png"
+  },
+  {
+    "revision": "866604d422c4ae2af2fcb92436e7ac80",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/waningCrescent.866604d4.png"
+  },
+  {
+    "revision": "256235ee78dd8b4f4a19908ab5d767c3",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/waningGibbous.256235ee.png"
+  },
+  {
+    "revision": "610a88c0c795fb6a33accb6e615185aa",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/waxingCrescent.610a88c0.png"
+  },
+  {
+    "revision": "bd6a45d95cae63ad4851f3dd61986d5d",
+    "url": "https://justin-blankenship.github.io/MAVIS/static/media/waxingGibbous.bd6a45d9.png"
+  }
+]);
