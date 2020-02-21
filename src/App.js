1ab2c5056a0d9f781d2632e594d1c9ab138fc880
Justin Blankenship <jblanken@unca.edu>
2020-02-21 16:54:07 -0500

    Add gh-pages library.

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import { HashRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Home';
 import Forecast from './Forecast';
 import Moon from './Moon';
