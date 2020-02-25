87f0f2c0c7b086a8b30ee2dbdbacf0745c840298
Justin Blankenship <jblanken@unca.edu>
2020-02-24 20:53:55 -0500

    Navbar styling.

--- src/components/Jumbotron.js
+++ src/components/Jumbotron.js
@@ -17,7 +17,7 @@ const Styles = styled.div`
 
 	.overlay {
 		background-color: #000;
-		opacity: 0.3;
+		opacity: 0.2;
 		position: absolute;
 		top: 0;
 		left: 0;
--- src/components/NavigationBar.js
+++ src/components/NavigationBar.js
@@ -3,18 +3,19 @@ import { Nav, Navbar } from 'react-bootstrap';
 import styled from 'styled-components';
 import mavisLogo from '../assets/mavisLogo.png';
 import './titleStyle.css';
+import '../Navbar.css';
 
 const Styles = styled.div`
 	.navbar {
-		background-color: #444;
+		background-color: #474747;
 		height: 100%;
 	}
 
 	.navbar-brand, .navbar-nav .nav-link {
-		color: #bbb;
+		color: #000;
 
 		&:hover {
-			color: white;
+			color: #15C8F9;
 		}
 	}
 
@@ -22,26 +23,27 @@ const Styles = styled.div`
 		width: 45px;
         height: 35px;
 	}
-
 `;
 
+
 export const NavigationBar = () => (
+
 	<Styles>
-		<Navbar expand="always">
+		<Navbar expand="lg">
 			<Navbar.Brand href="/">
 				<img
-        			src={mavisLogo}
-        			className="logoImage ml-4"
-        			alt="MAVIS Logo"
-      			/>
-      			<p className="navbarTitle m-auto pl-3">MAVIS</p>
+	    			src={mavisLogo}
+	    			className="logoImage ml-4"
+	    			alt="MAVIS Logo"
+	  			/>
+	  			<p className="navbarTitle m-auto pl-3">MAVIS</p>
 			</Navbar.Brand>
 
 			<Navbar.Toggle aria-controls="basic-navbar-nav" />
 			<Navbar.Collapse id="basic-navbar-nav">
 				<Nav className="ml-auto text-right">
 
-					<Nav.Item data-target="#sidenav-collapse-main">
+					<Nav.Item>
 						<Nav.Link href="#/">Home</Nav.Link>
 					</Nav.Item>
 
@@ -52,8 +54,11 @@ export const NavigationBar = () => (
 					<Nav.Item>
 						<Nav.Link href="#/moon">Moon</Nav.Link>
 					</Nav.Item>
+					
 				</Nav>
+
 			</Navbar.Collapse>
 		</Navbar>
 	</Styles>
+	
 )
